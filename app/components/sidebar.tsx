b8ff2aabe78878213322fe80ca225e431d8c7608
chuckxue <38340679+ChuckXue@users.noreply.github.com>
2023-05-31 05:28:26 +0000

    更新标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT-XCG</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          流量有限,切勿泄露,谢谢配合！！！
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
